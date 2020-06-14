bf9d00f3de3b43f043c32b9b6b09c633ee14efff
Alexey Murentsev <murentsev@icloud.com>
2020-06-14 16:52:39 +0300

    Initial Commit

---
+++ Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Алексей Муренцев on 14.06.2020.
+//  Copyright © 2020 Алексей Муренцев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
