35aa8d4f8d2b24fcf3e00f6461e88b47321ceac9
Alexey Murentsev <murentsev@icloud.com>
2020-06-14 17:57:09 +0300

    login view

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -10,11 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBAction func signinPressed(_ sender: UIButton) {
+        guard let login = loginTextField.text, let password = passwordTextField.text else { return }
+        print("login: \(login) Password: \(password)")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    
+    @objc func keyboardWillShow(notification: Notification) {
+        guard let kbSize = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else { return }
+        
+        scrollView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: kbSize.height, right: 0)
+    }
+    
+    @objc func keyboardWillHide(notification: Notification) {
+        scrollView.contentInset = .zero
+    }
+    
+    @IBAction func scrollTapped(_ gesture: UIGestureRecognizer) {
+        scrollView.endEditing(true)
+    }
 }
 
